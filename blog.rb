66294054bfe0be807166db97acde47c992ef3110
ほと <mail@hoto.me>
2016-07-19 00:02:48 +0900

    post method

--- blog.rb
+++ blog.rb
@@ -3,24 +3,30 @@ require 'dotenv'
 
 Dotenv.load
 
-driver = Selenium::WebDriver.for :chrome
+@driver = Selenium::WebDriver.for :chrome
 
-# ログイン
-driver.navigate.to 'https://www.hatena.ne.jp/login'
-form = driver.find_element(:css, 'form[action="/login"]')
-form.find_element(:css, 'input[name="name"]').send_keys(ENV['HATENA_ID'])
-form.find_element(:css, 'input[name="password"]').send_keys(ENV['HATENA_PASSWORD'])
-form.find_element(:css, 'input[type="submit"]').click
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-wait.until { driver.current_url == 'http://www.hatena.ne.jp/' }
+def login(id, password)
+  @driver.navigate.to 'https://www.hatena.ne.jp/login'
+  form = @driver.find_element(:css, 'form[action="/login"]')
+  form.find_element(:css, 'input[name="name"]').send_keys(id)
+  form.find_element(:css, 'input[name="password"]').send_keys(password)
+  form.find_element(:css, 'input[type="submit"]').click
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  wait.until { @driver.current_url == 'http://www.hatena.ne.jp/' }
+end
 
-# 投稿
-driver.navigate.to "http://blog.hatena.ne.jp/#{ENV['HATENA_ID']}/#{ENV['HATENA_BLOG_ID']}/edit"
-form = driver.find_element(:id, 'edit-form')
-form.find_element(:css, '[data-support-type="editor-option"]').click
-form.find_element(:id, 'title').send_keys('タイトル')
-form.find_element(:id, 'body').send_keys('本文')
-form.find_element(:id, 'datetime-input-date').send_keys('2016-01-01')
-form.find_element(:id, 'customurl-input').send_keys('test')
+def post(title, body, date, url)
+  @driver.navigate.to "http://blog.hatena.ne.jp/#{ENV['HATENA_ID']}/#{ENV['HATENA_BLOG_ID']}/edit"
+  form = @driver.find_element(:id, 'edit-form')
+  form.find_element(:css, '[data-support-type="editor-option"]').click
+  form.find_element(:id, 'title').send_keys(title)
+  form.find_element(:id, 'body').send_keys(body)
+  form.find_element(:id, 'datetime-input-date').send_keys(date)
+  form.find_element(:id, 'customurl-input').send_keys(url)
+  # TODO submit
+end
 
-#driver.quit
+login(ENV['HATENA_ID'], ENV['HATENA_PASSWORD'])
+post('タイトル', '本文', '2016-01-01', 'test')
+
+#@driver.quit
